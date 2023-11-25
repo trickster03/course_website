41eb17696949703b2938925fd6918f1558ca8129
Ramsha Iqbal <ramshaiqbal4@gmail.com>
2023-11-26 00:11:21 +0530

    added app page

--- course/src/App.js
+++ course/src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home';
+import About from './pages/About/About'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar />
+    <Home />
+    <About />
+  <Footer />
     </div>
   );
 }
