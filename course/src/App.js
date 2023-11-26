cf85313611b7a0e0624228f3f791b23f4c35e3b9
Ramsha Iqbal <ramshaiqbal4@gmail.com>
2023-11-26 17:41:12 +0530

    updated navbar and footer

--- course/src/App.js
+++ course/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home';
-import About from './pages/About/About'
-import Footer from './components/Footer';
+import About from './pages/About/About';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div>
     <Navbar />
     <Home />
-    <About />
+     <About />
   <Footer />
     </div>
   );
--- course/src/components/Footer/Footer.jsx
+++ course/src/components/Footer/Footer.jsx
@@ -11,26 +11,30 @@ const Footer = () => {
 
 
 
-                        <p>LNMIIT <br />Rupa Ki Nangal, Post Sumel
-                            <br />Via jamdoli<br />Jaipur,
-                            India</p>
-                   
+                <p>LNMIIT <br />Rupa Ki Nangal, Post Sumel,
+                        Via Jamdoli<br />Jaipur,
+                        India</p>
+
 
                 </div>
                 <div className={Footcss.right}>
-                    
-                        <p>Email: Saunakkushwaha@gmail.com<br />
-                            Contact: +917007623090</p>
-                    
-                            
-                </div>
-               
-            </div>
-            <div className={Footcss.pgb}></div>
-            <br></br>
-            <h4 className={Footcss.heading}>Copyright 2023 @ All rights reserved</h4>
-           
 
+                    <p>Email: Saunakkushwaha@gmail.com<br />
+                        Contact: +917007623090</p>
+
+
+                </div>
+
+            </div>
+
+            <div className={Footcss.cpyrght}>
+                <div className={Footcss.cpytext}>
+                    <h3>Copyright 2023 @ All rights reserved</h3>
+                    <p> Disclaimer:
+                        All content on this website is protected by copyright law. Users are prohibited from unauthorized use or reproduction of any material without explicit permission. [Your Website Name] reserves the right to take legal action against any infringement.</p>
+                </div>
+
+            </div>
         </div>
 
     )
