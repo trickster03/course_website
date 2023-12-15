7340677f5bb8ce15aa4eb147572747f0d0a67005
shanatanu002 <bajpaishantanu2002@gmail.com>
2023-12-15 21:34:55 +0530

    NEW COMMIT

--- course/src/components/Footer/Footer.jsx
+++ course/src/components/Footer/Footer.jsx
@@ -8,10 +8,6 @@ const Footer = () => {
             <h1 className={Footcss.heading}>Contact Us</h1>
             <div className={Footcss.footContainer}>
                 <div className={Footcss.left}>
-
-
-
-<<<<<<< Updated upstream
                         <p>LNMIIT <br />Rupa Ki Nangal, Post Sumel
                             <br />Via jamdoli<br />Jaipur,
                             India</p>
@@ -32,40 +28,29 @@ const Footer = () => {
             <h4 className={Footcss.heading}>Copyright 2023 @ All rights reserved</h4>
            
 =======
+=======
+>>>>>>> Stashed changes
                     <p>LNMIIT <br />Rupa Ki Nangal, Post Sumel,
                         Via Jamdoli<br />Jaipur,
                         India</p>
-
-
                 </div>
                 <div className={Footcss.right}>
-
                     <p>Email: Saunakkushwaha@gmail.com<br />
                         Contact: +917007623090</p>
-
-
                 </div>
 
-            </div>
+            </div >
 
-            <div className={Footcss.cpyrght}>
-                <div className={Footcss.cpytext}>
-                    <h3>Copyright 2023 @ All rights reserved</h3>
-                    <p> Disclaimer:
-                        All content on this website is protected by copyright law. Users are prohibited from unauthorized use or reproduction of any material without explicit permission. [Your Website Name] reserves the right to take legal action against any infringement.</p>
-                </div>
-
-            </div>
-
->>>>>>> Stashed changes
-
+    <div className={Footcss.cpyrght}>
+        <div className={Footcss.cpytext}>
+            <h3>Copyright 2023 @ All rights reserved</h3>
+            <p> Disclaimer:
+                All content on this website is protected by copyright law. Users are prohibited from unauthorized use or reproduction of any material without explicit permission. [Your Website Name] reserves the right to take legal action against any infringement.</p>
         </div>
+
+    </div> </div >
 
     )
 }
 
-<<<<<<< Updated upstream
 export default Footer
-=======
-export default Footer
->>>>>>> Stashed changes
