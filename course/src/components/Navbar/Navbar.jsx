454dad90f9c21970fa108dc691b75b490f0dd9ef
shanatanu002 <bajpaishantanu2002@gmail.com>
2023-12-22 21:30:18 +0530

    Frontend changes

--- course/src/components/Navbar/Navbar.jsx
+++ course/src/components/Navbar/Navbar.jsx
@@ -6,51 +6,50 @@ import { useState } from 'react';
 
 
 
-const goToCourses=()=>{
+const goToCourses = () => {
   window.scrollTo({ top: 650, left: 0, behavior: "smooth" });
 }
 
 
 
-const goToTop=()=>{
+const goToTop = () => {
   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 }
 
-const goToContacts=()=>{
+const goToContacts = () => {
   window.scrollTo({ top: 2000, left: 0, behavior: "smooth" });
 }
 
 
 const Navbar = () => {
-const [ShowMenu,setShowMenu]=useState(false);
- 
+  const [ShowMenu, setShowMenu] = useState(false);
 
-const handleClick=()=>
-{
-  setShowMenu(!ShowMenu);
-  console.log('clicked')
-}
- 
+
+  const handleClick = () => {
+    setShowMenu(!ShowMenu);
+    console.log('clicked')
+  }
+
   return (
     <>
-    <div className={Navcss.main}>
-    <div className={Navcss.background}>
-        <Link  className={Navcss.links} onClick={goToTop} to="/">Home</Link> 
-       <Link className={Navcss.links} to='/' onClick={goToCourses}>Courses</Link>
-       <Link className={Navcss.links}  to='/' onClick={goToContacts}>Contact Us</Link>
-       <Link className={Navcss.links} to="/credits">Credits</Link>
-       <div className={Navcss.menu} onClick={handleClick}><GiHamburgerMenu size={30} style={{color: 'white'}} /></div>
-    </div>
- {ShowMenu &&(
-  <div className={`${Navcss.open} ${ShowMenu ? 'show' : ''}`}>
-    <Link  className={Navcss.links1} onClick={goToTop} to="/">Home</Link> 
-       <Link className={Navcss.links1} to='/' onClick={goToCourses}>Courses</Link>
-       <Link className={Navcss.links1}  to='/' onClick={goToContacts}>Contact Us</Link>
-       <Link className={Navcss.links1} to="/credits">Credits</Link>
- </div>
-) }
-</div>
-     </>
+      <div className={Navcss.main}>
+        <div className={Navcss.background}>
+          <Link className={Navcss.links} onClick={goToTop} to="/">Home</Link>
+          <Link className={Navcss.links} to='/' onClick={goToCourses}>Courses</Link>
+          <Link className={Navcss.links} to='/' onClick={goToContacts}>Contact Us</Link>
+          <Link className={Navcss.links} to="/credits">Credits</Link>
+          <div className={Navcss.menu} onClick={handleClick}><GiHamburgerMenu size={30} style={{ color: 'white' }} /></div>
+        </div>
+        {ShowMenu && (
+          <div className={`${Navcss.open} ${ShowMenu ? 'show' : ''}`}>
+            <Link className={Navcss.links1} onClick={goToTop} to="/">Home</Link>
+            <Link className={Navcss.links1} to='/' onClick={goToCourses}>Courses</Link>
+            <Link className={Navcss.links1} to='/' onClick={goToContacts}>Contact Us</Link>
+            <Link className={Navcss.links1} to="/credits">Credits</Link>
+          </div>
+        )}
+      </div>
+    </>
   )
 
 }
